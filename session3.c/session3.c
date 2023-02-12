f3f89a7e9a62491329f3c890cd5a8d994f89619f
merrylearninggithub <xc2081@nyu.edu>
2023-02-12 18:47:37 -0500

    modified

--- session3.c/session3.c
+++ session3.c/session3.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <limits.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]){
   int test = 0x0F; /* 15 in hexadecimal */
 
   printf("\n");
@@ -14,16 +14,16 @@ int main(int argc, char* argv[])
 
   /***********************************************************
    * data type sizes 
-   ************************************************************
+   ************************************************************/
   int max_int = 0x7FFFFFFF;                     /* write the max int in hexadecimal notation */
-  int min_int = 0x7FFFFFFF;                /* write the min int in hexadecimal representation*/
-  short min_sint = -8000;                     /* write the minimum short int in hexadecimal representation */
+  int min_int = -0x80000000;                /* write the min int in hexadecimal representation*/
+  short min_sint = -0x8000;                     /* write the minimum short int in hexadecimal representation */
   short max_sint = 0x7FFF;                      /* write the maximum short int in hexadecimal representation */
-  long min_lint = 0x7FFFFFFF;      /* write the minimum long int in hexadecimal representation */
-  long max_lint = 0x7FFFFFFF;           /* write the maximum long  int in hexadecimal representation */ 
+  long min_lint = -0x8000000000000000;      /* write the minimum long int in hexadecimal representation */
+  long max_lint = 0x7FFFFFFFFFFFFFFF;           /* write the maximum long  int in hexadecimal representation */ 
   unsigned int max_uint = 0xFFFFFFFF;           /* write the usigned max int in hexadecimal notation */
   unsigned short max_usint = 0xFFFF;            /* write the unsigned max short int in hexadecimal notation */
-  unsigned long max_ulint = 0xFFFFFFFF; /* write the unsigned max long int in hexadecimal notation */
+  unsigned long max_ulint = 0xFFFFFFFFFFFFFFFF; /* write the unsigned max long int in hexadecimal notation */
 
   printf("max int is %d\n", max_int);
   printf("min int is %d\n", min_int);
